d75cc4699751c01169d22f8b42164419a85972cf
gaoshaoyun <276185876@qq.com>
2020-10-21 21:04:50 +0800

    save break

--- src/index.js
+++ src/index.js
@@ -1,71 +1,20 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
 
-// import React from "./kreact/";
-// import ReactDOM, {useState} from "./kreact/react-dom";
+import React from "./kreact/";
+import ReactDOM from "./kreact/react-dom";
 // import Component from "./kreact/Component";
+
 import "./index.css";
-
-class ClassComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {count: 0};
-  }
-  // *
-  // *
-  // todo
-  static getDerivedStateFromProps(props, state) {
-    // getDerivedStateFromProps 会在调用 render 方法之前调用，
-    //并且在初始挂载及后续更新时都会被调用。
-    //它应返回一个对象来更新 state，如果返回 null 则不更新任何内容。
-    const {count} = state;
-    console.log("getDerivedStateFromProps", count);
-    return count < 3 ? null : {count: 0};
-  }
-
-  render() {
-    const {count} = this.state;
-    return (
-      <div className="border">
-        <h3>ClassComponent</h3>
-        <p>{this.props.name}</p>
-        <button
-          onClick={() => {
-            this.setState({count: count + 1});
-            //this.setState({count: count + 2});
-          }}>
-          add count: {count}
-        </button>
-      </div>
-    );
-  }
-}
-
-function FunctionComponent(props) {
-  const [count, setCount] = React.useState(0);
-  return (
-    <div className="border">
-      <h3>FunctionComponent</h3>
-      <p>{props.name}</p>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}>
-        add count: {count}
-      </button>
-    </div>
-  );
-}
 
 const jsx = (
   <div className="border">
+    <h1>慢 慢 慢</h1>
     <p>开课吧</p>
     <a href="https://www.kaikeba.com/">开课吧</a>
-    <FunctionComponent name="函数组件" />
-    <ClassComponent name="类组件" />
   </div>
 );
 
+// 经过babel-loader编译，jsx就是React.createElement(...)函数执行
 ReactDOM.render(jsx, document.getElementById("root"));
-
 console.log("version-sy-log", React.version); //sy-log
--- src/kreact/index.js
+++ src/kreact/index.js
@@ -1,38 +1,7 @@
-import {TEXT} from "./const";
-import Component from "./Component";
-
-// 创建react element，并返回
-function createElement(type, config, ...children) {
-  if (config) {
-    delete config.__self;
-    delete config.__source;
-  }
-  // 这个地方我们自己写的没有考虑细节，比如key、ref等
-  const props = {
-    ...config,
-    children: children.map(child =>
-      typeof child === "object" ? child : createTextNode(child)
-    )
-  };
-  delete props.key;
-  return {
-    key: (config && config.key) || "",
-    type,
-    props
-  };
-}
-
-function createTextNode(text) {
-  return {
-    type: TEXT,
-    props: {
-      children: [],
-      nodeValue: text
-    }
-  };
+function createElement() {
+  return {};
 }
 
 export default {
-  createElement,
-  Component
+  createElement
 };
