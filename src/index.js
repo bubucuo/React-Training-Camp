35bb2505075fbb976b092c97d2bd823fafbba5ea
gaoshaoyun <276185876@qq.com>
2020-10-21 22:07:15 +0800

    save day1

--- src/index.js
+++ src/index.js
@@ -7,14 +7,33 @@ import ReactDOM from "./kreact/react-dom";
 
 import "./index.css";
 
+function FunctionComponent(props) {
+  return <div className="border">FunctionComponent-{props.name}</div>;
+}
+
 const jsx = (
   <div className="border">
     <h1>慢 慢 慢</h1>
     <p>开课吧</p>
     <a href="https://www.kaikeba.com/">开课吧</a>
+    <FunctionComponent name="函数组件" />
   </div>
 );
 
 // 经过babel-loader编译，jsx就是React.createElement(...)函数执行
 ReactDOM.render(jsx, document.getElementById("root"));
 console.log("version-sy-log", React.version); //sy-log
+
+// 原生标签节点
+// 文本节点
+// 函数组件节点
+
+// *
+// !
+// ?
+// todo 这里不是要做的事情 只是大家对黄色敏感而已
+// todo React.createElement什么时候调用了？
+// * 因为react里我写的jsx， jsx经过babel-loader编译，会变成React.createElement(...)函数的执行
+
+// todo 函数组件和类组件区别?
+//
